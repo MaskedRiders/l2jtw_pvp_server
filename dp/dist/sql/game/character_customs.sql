6609290e9e99b71a4b4a790e64e415e0e06b33f6
MaskedRiderW <nakamura.shingo+l2j@gmail.com>
2014-10-28 20:45:59 +0900

    trading_pointカラムの名前に余計なスペースが入ってしまっていたので修正 Signed-off-by: MaskedRiderW

--- dp/dist/sql/game/character_customs.sql
+++ dp/dist/sql/game/character_customs.sql
@@ -6,9 +6,9 @@ CREATE TABLE `character_customs` (
   `battle_score_best_date` varchar(19)                              COMMENT '戦闘スコアベスト時刻(YYYY/MM/DD hh:nn:ss)',
   `battle_log`             mediumtext                               COMMENT '戦闘記録
 (kill,charId,battle_score,date;death,charId,battle_score,date...)',
-  `tvt_score`              bigint(20) unsigned           default '0' COMMENT 'TvTスコア',
+  `tvt_score`              bigint(20) unsigned          default '0' COMMENT 'TvTスコア',
   `tvt_score_log`          mediumtext                               COMMENT 'TvTスコア記録(battle_score,date;battle_score,date...)',
-  `	trading_point`          bigint(20) unsigned          default '0' COMMENT '交換用ポイント',
+  `trading_point`          bigint(20) unsigned          default '0' COMMENT '交換用ポイント',
   PRIMARY KEY  (`charId`),
   KEY `idx_chrId_battle_score` (`charId`,`battle_score`) USING BTREE,
   KEY `idx_chrId_tvt_score`    (`charId`,`tvt_score`) USING BTREE
