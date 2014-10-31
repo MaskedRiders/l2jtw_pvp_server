133a17b58ae80cf695a6d260f0ceb95d40e86127
MaskedRiderW <nakamura.shingo+l2j@gmail.com>
2014-10-31 20:51:11 +0900

    いったん削除して追加する形に。 Signed-off-by: MaskedRiderW

--- dp/dist/sql/game/updates/20141031update.sql
+++ dp/dist/sql/game/updates/20141031update.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `character_customs`
-MODIFY COLUMN `battle_score_best_date`  int(10) NULL DEFAULT NULL COMMENT '戦闘スコアベスト時刻' AFTER `battle_score_best`;
+DROP COLUMN `battle_score_best_date`,
+ADD COLUMN `battle_score_best_date`  int(10) NULL COMMENT '戦闘スコアベスト時刻' AFTER `battle_score_best`;
