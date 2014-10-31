8254484a4a4c69e07a759c60957b5619ef4bfe75
MaskedRiderW <nakamura.shingo+l2j@gmail.com>
2014-10-31 21:07:01 +0900

    character_customsテーブルにゾンビカラムを追加 Signed-off-by: MaskedRiderW

--- dp/dist/sql/game/updates/20141031update.sql
+++ dp/dist/sql/game/updates/20141031update.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `character_customs`
-DROP COLUMN `battle_score_best_date`,
-ADD COLUMN `battle_score_best_date`  int(10) NULL COMMENT '戦闘スコアベスト時刻' AFTER `battle_score_best`;
+DROP COLUMN `battle_score_best`,
+ADD COLUMN `battle_score_best`  int(10) NULL DEFAULT NULL COMMENT '戦闘スコアベスト' AFTER `battle_score`,
+ADD COLUMN `pvp_death_date`  int(10) NULL COMMENT 'PvP死亡時刻' AFTER `tvt_score_log`,
+ADD COLUMN `pvp_zombie`  tinyint(1) NULL COMMENT 'ゾンビ' AFTER `pvp_death_date`;
+
